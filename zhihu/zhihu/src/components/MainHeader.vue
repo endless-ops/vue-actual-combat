51fc21028c792ecd96ee5ac4097f568679549a0d
孟德方 <85704791+endless-ops@users.noreply.github.com>
2022-08-03 02:51:49 +0800

    vue 基础与实战

---
+++ zhihu/zhihu/src/components/MainHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="main-header">
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
