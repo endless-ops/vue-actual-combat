ab90d2581a228b7525960926f3fdb553acabaef5
孟德方 <85704791+endless-ops@users.noreply.github.com>
2022-08-03 12:57:19 +0800

    vue 实战

--- zhihu/zhihu/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import installElementPlus from './plugins/element'
-
-const app = createApp(App)
-installElementPlus(app)
-app.use(router).mount('#app')
